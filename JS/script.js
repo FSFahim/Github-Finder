c2db192ff265dca08b85ac734fffc64a3141af27
FSFahim <farhanshahriar3728@gmail.com>
2024-08-12 16:27:18 +0600

    Fetch User Info from API

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,28 @@
+//Define UI elements
 let searchBtn = document.querySelector("#searchBtn");
-let searchUser = document.querySelector("searchUser");
+let searchUser = document.querySelector("#searchUser");
 
-  
+//Define Event Listeners
+searchBtn.addEventListener("click", findUser);
+
+//Define functions
+function findUser(e) {
+  let userText = searchUser.value;
+  if (userText == "") {
+    alert("Input field is empty!");
+    //Clear Profile
+  } else {
+    //Fetch API
+    fetch(`https://api.github.com/users/${userText}`)
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+        if (data.message == "Not Found") {
+          //Show Alert
+        } else {
+          //Show Profile
+          searchUser.value = "";
+        }
+      });
+  }
+}
