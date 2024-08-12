44530c8edd605e86141064ad8bd1630396ec6f05
FSFahim <farhanshahriar3728@gmail.com>
2024-08-12 16:42:16 +0600

    UI class done

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 //Define UI elements
 let searchBtn = document.querySelector("#searchBtn");
 let searchUser = document.querySelector("#searchUser");
+let ui = new UI();
 
 //Define Event Listeners
 searchBtn.addEventListener("click", findUser);
@@ -9,18 +10,20 @@ searchBtn.addEventListener("click", findUser);
 function findUser(e) {
   let userText = searchUser.value;
   if (userText == "") {
-    alert("Input field is empty!");
+    ui.showAlert("Input field is empty!", "alert alert-danger");
     //Clear Profile
+    ui.clearProfile();
   } else {
     //Fetch API
     fetch(`https://api.github.com/users/${userText}`)
       .then((result) => result.json())
       .then((data) => {
-        console.log(data);
         if (data.message == "Not Found") {
           //Show Alert
+          ui.showAlert("User not Found!", "alert alert-danger");
         } else {
           //Show Profile
+          ui.showProfile(data);
           searchUser.value = "";
         }
       });
